64a8d6da705fdbcb1b74c8d8137348fd6bdaed32
yourname <yourname@gmail.com>
2016-10-25 23:30:00 +0800

    Modify for omni

--- 1.FetchSource.sh
+++ 1.FetchSource.sh
@@ -1,8 +1,11 @@
 mkdir ~/bin
-curl https://storage.googleapis.com/git-repo-downloads/repo > bin/repo
-chmod a+x bin/repo
-mkdir zs570kl_cm14
-cd zs570kl_cm14
-repo init -u https://github.com/CyanogenMod/android.git -b staging/cm-14.0
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+mkdir zs570kl
+cd zs570kl
+git config --global user.email "yourname@gmail.com"
+git config --global user.name "yourname"
+repo init -u https://github.com/omnirom/android.git -b android-7.0
+mkdir .repo/local_manifests
 cp ../roomservice.xml .repo/local_manifests/roomservice.xml
 repo sync
