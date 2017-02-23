ab2f84873f8fc0424a640f58b58b39db303685ab
Jung-Chi Wang <snowwolf725@users.noreply.github.com>
2017-02-23 10:37:21 +0800

    Update 1.FetchSource.sh

--- 1.FetchSource.sh
+++ 1.FetchSource.sh
@@ -3,6 +3,7 @@ curl https://storage.googleapis.com/git-repo-downloads/repo > bin/repo
 chmod a+x bin/repo
 mkdir zs570kl_cm14
 cd zs570kl_cm14
-repo init -u https://github.com/CyanogenMod/android.git -b staging/cm-14.0
+repo init -u https://github.com/CyanogenMod/android.git -b cm-14.1
+mkdir .repo/local_manifests
 cp ../roomservice.xml .repo/local_manifests/roomservice.xml
 repo sync
